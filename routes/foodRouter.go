3e3f38c03af400a187349910723d64759b1ba524
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2026-03-16 22:35:55 +0530

    added user routes and food routes

---
+++ routes/foodRouter.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "restaurant-management-system/controllers"
+)
+
+func foodRoutes (incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/foods", controller.getFoods())
+	incomingRoutes.GET("/foods/:food_id", controller.getFood())
+	incomingRoutes.POST("/foods", controller.createFood())
+	incomingRoutes.PATCH("/foods/:food_id", controller.updateFood())
+}
