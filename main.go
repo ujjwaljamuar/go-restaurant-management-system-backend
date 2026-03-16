cfa6db031941df6d95e731c9a8ac27fc1e845f09
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2026-03-16 22:35:03 +0530

    setup main file, made connection with mongodb

--- main.go
+++ main.go
@@ -1,2 +1,34 @@
 package main
 
+import (
+	"os"
+	"github.com/gin-gonic/gin"
+	"restaurant-management-system/database"
+	"restaurant-management-system/routes"
+	"restaurant-management-system/middlewares"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main ()  {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UseRoutes(router)
+	router.Use(middlewares.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+}
